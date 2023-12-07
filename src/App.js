f7eea93bf0774d66eaee3eb5e0f0988d1594f8bf
muhammad-waqar-aciano <muhammad.waqar.aciano@outlook.com>
2023-12-07 19:40:25 +0500

    add deploy url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import pdfUrl from "./aciano-company-profile.pdf";
 
 function App() {
   // const pdfUrl = 'https://example.com/your-pdf-file.pdf';
-
+//deploy URL: https://aciano-policies.netlify.app/
   return (
     <div className="App">
       <PDFViewer pdfUrl={pdfUrl} />
